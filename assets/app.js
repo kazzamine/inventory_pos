d6f0a1377681558956dbc09eec45dad31cb6f1b4
kazzamine <aminkazzout@gmail.com>
2024-02-07 17:06:13 +0100

    updated packages

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+import '.';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +7,3 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
