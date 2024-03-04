458adda8337db01b0ed525bb72709793a21339e4
kazzamine <aminkazzout@gmail.com>
2024-03-04 15:54:42 +0100

    fix display chart,add cities select options for user creation -from cities api-

--- src/Form/CreateUserForm.php
+++ src/Form/CreateUserForm.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Form;
+use http\Message\Body;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -9,11 +10,20 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\UX\Dropzone\Form\DropzoneType;
 
 class CreateUserForm extends AbstractType{
+    private $httpClient;
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $cities = $this->fetchCityData($options['cities_api']);
+
         $builder
             ->add('picture', FileType::class, [
                 'label' => 'profile Picture    ',
@@ -39,8 +49,8 @@ class CreateUserForm extends AbstractType{
             ->add('phone',TextType::class,
                 array('attr'=>array('class'=>'form-control'))
             )
-            ->add('city',TextType::class,
-                array('attr'=>array('class'=>'form-control'))
+            ->add('city',ChoiceType::class,
+                array('choices'=>$cities,'attr'=>array('class'=>'form-control'))
             )
             ->add('roleId',ChoiceType::class,
                 array('choices'=>array('ROLE_USER'=>'3','ROLE_ADMIN'=>'4'), 'attr'=>array('class'=>'form-control'))
@@ -48,7 +58,7 @@ class CreateUserForm extends AbstractType{
                 array('attr'=>array('class'=>'form-control'))
             )
             ->add('createuser', SubmitType::class,
-                array('attr'=>array('class'=>'btn btn-primary d-block w-100'))
+                array('attr'=>array('class'=>'btn btn-primary d-block w-100','data-bs-toggle'=>'modal', 'data-bs-target'=>'#orderload'))
             )
         ;
     }
@@ -56,6 +66,21 @@ class CreateUserForm extends AbstractType{
     {
         $resolver->setDefaults([
             'csrf_protection' => true,
+            'cities_api' => 'https://countriesnow.space/api/v0.1/countries/cities',
         ]);
+    }
+
+    private function fetchCityData(string $apiEndpoint): array
+    {
+        $response = $this->httpClient->request('POST', $apiEndpoint,[
+            'body'=>['country'=>'morocco']
+        ]);
+        $data = $response->toArray();
+        $choices = [];
+        foreach ($data['data'] as $city) {
+            $choices[$city] = $city;
+        }
+
+        return $choices;
     }
 }
